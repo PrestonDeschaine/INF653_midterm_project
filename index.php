b57c88c4158012777012192e0f7d9bff4f3f49d2
Preston Deschaine <prestonpd11@gmail.com>
2024-03-17 14:21:54 -0500

    Docker implementation

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<p>This is the display change</p>
+<? php
+
+declare(strict_types=1);
+
+echo '<pres>';
+print_r(get_env('SITE_URL'));
+echo '<br>';
+print_r($_SERVER);
+echo '</pre>';
+
+phpinfo();
